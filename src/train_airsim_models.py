bef7e36e6696c8795d8391a43510a41b8d67d346
hwelsters <hwelsters>
2024-09-02 17:09:05 -0700

    init

--- src/train_airsim_models.py
+++ src/train_airsim_models.py
@@ -1,10 +1,11 @@
 from object_detection.detr import train_base_model, train_peft_model_ia3, train_peft_model_lora, train_peft_model_lntuning
 import os
 
-from training_configs import AirsimConfig
+from training_configs import AirsimObjectDetectionConfig
 
-for dataset in AirsimConfig.datasets:
-    for checkpoint in AirsimConfig.checkpoints:
+for checkpoint in AirsimObjectDetectionConfig.checkpoints:
+    for dataset in AirsimObjectDetectionConfig.datasets:
+
         output_dir = os.path.join(
             "output",
             dataset,
@@ -18,35 +19,35 @@ for dataset in AirsimConfig.datasets:
 
         train_base_model(
             checkpoint=checkpoint,
-            id2label=AirsimConfig.id2label,
-            label2id=AirsimConfig.label2id,
+            id2label=AirsimObjectDetectionConfig.id2label,
+            label2id=AirsimObjectDetectionConfig.label2id,
             dataset_dir=dataset_dir,
             output_dir=output_dir_base,
         )
 
-        output_base_model_path = os.path.join(output_dir_base, "model.pth")
+        # output_base_model_path = os.path.join(output_dir_base, "model.pth")
         
-        # Train PEFT models ========================================
-        train_peft_model_lora(
-            checkpoint=output_base_model_path,
-            id2label=AirsimConfig.id2label,
-            label2id=AirsimConfig.label2id,
-            dataset_dir=dataset_dir,
-            output_dir=os.path.join(output_dir, "lora")
-        )
+        # # Train PEFT models ========================================
+        # train_peft_model_lora(
+        #     checkpoint=output_base_model_path,
+        #     id2label=AirsimObjectDetectionConfig.id2label,
+        #     label2id=AirsimObjectDetectionConfig.label2id,
+        #     dataset_dir=dataset_dir,
+        #     output_dir=os.path.join(output_dir, "lora")
+        # )
         
-        train_peft_model_ia3(
-            checkpoint=output_base_model_path,
-            id2label=AirsimConfig.id2label,
-            label2id=AirsimConfig.label2id,
-            dataset_dir=dataset_dir,
-            output_dir=os.path.join(output_dir, "ia3"),
-        )
+        # train_peft_model_ia3(
+        #     checkpoint=output_base_model_path,
+        #     id2label=AirsimObjectDetectionConfig.id2label,
+        #     label2id=AirsimObjectDetectionConfig.label2id,
+        #     dataset_dir=dataset_dir,
+        #     output_dir=os.path.join(output_dir, "ia3"),
+        # )
 
-        train_peft_model_lntuning(
-            checkpoint=output_base_model_path,
-            id2label=AirsimConfig.id2label,
-            label2id=AirsimConfig.label2id,
-            dataset_dir=dataset_dir,
-            output_dir=os.path.join(output_dir, "lntuning"),
-        )
+        # train_peft_model_lntuning(
+        #     checkpoint=output_base_model_path,
+        #     id2label=AirsimObjectDetectionConfig.id2label,
+        #     label2id=AirsimObjectDetectionConfig.label2id,
+        #     dataset_dir=dataset_dir,
+        #     output_dir=os.path.join(output_dir, "lntuning"),
+        # )
--- src/training_configs.py
+++ src/training_configs.py
@@ -1,4 +1,4 @@
-class AirsimConfig:
+class AirsimObjectDetectionConfig:
     id2label = {0: "construction", 1: "nature", 2: "pedestrian", 3: "vehicle"}
     label2id = {v: k for k, v in id2label.items()}
 
@@ -7,8 +7,8 @@ class AirsimConfig:
         "hustvl/yolos-tiny",
         "hustvl/yolos-small",
         "hustvl/yolos-base",
-        "facebook/detr-resnet-50",
-        "facebook/detr-resnet-101",
-        "google/owlvit-base-patch32",
-        "google/owlvit-large-patch32",
+        # "facebook/detr-resnet-50",
+        # "facebook/detr-resnet-101",
+        # "google/owlvit-base-patch32",
+        # "google/owlvit-large-patch32",
     ]
