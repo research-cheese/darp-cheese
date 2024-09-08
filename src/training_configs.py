b44f56124f52c8faecbe0c796d3ebeb2c053a09c
hwelsters <hwelsters>
2024-09-08 00:46:29 -0700

    a hard day's work

--- src/training_configs.py
+++ src/training_configs.py
@@ -2,23 +2,23 @@ class AirsimObjectDetectionConfig:
     id2label = {0: "construction", 1: "nature", 2: "pedestrian", 3: "vehicle"}
     label2id = {v: k for k, v in id2label.items()}
 
-    base_dataset = "caleb/mcai/airsim-data/mixed/base"
+    base_dataset = "data/mcai/airsim-data/mixed/base"
     datasets = [
-        "caleb/mcai/airsim-data/mixed/dust-10",
-        "caleb/mcai/airsim-data/mixed/dust-100",
-        "caleb/mcai/airsim-data/mixed/dust-1000",
-        "caleb/mcai/airsim-data/mixed/fog-10",
-        "caleb/mcai/airsim-data/mixed/fog-100",
-        "caleb/mcai/airsim-data/mixed/fog-1000",
-        "caleb/mcai/airsim-data/mixed/snow-10",
-        "caleb/mcai/airsim-data/mixed/snow-100",
-        "caleb/mcai/airsim-data/mixed/snow-1000",
-        "caleb/mcai/airsim-data/mixed/rain-10",
-        "caleb/mcai/airsim-data/mixed/rain-100",
-        "caleb/mcai/airsim-data/mixed/rain-1000",
-        "caleb/mcai/airsim-data/mixed/maple_leaf-10",
-        "caleb/mcai/airsim-data/mixed/maple_leaf-100",
-        "caleb/mcai/airsim-data/mixed/maple_leaf-1000",
+        "data/mcai/airsim-data/mixed/dust-10",
+        "data/mcai/airsim-data/mixed/dust-100",
+        "data/mcai/airsim-data/mixed/dust-1000",
+        "data/mcai/airsim-data/mixed/fog-10",
+        "data/mcai/airsim-data/mixed/fog-100",
+        "data/mcai/airsim-data/mixed/fog-1000",
+        "data/mcai/airsim-data/mixed/snow-10",
+        "data/mcai/airsim-data/mixed/snow-100",
+        "data/mcai/airsim-data/mixed/snow-1000",
+        "data/mcai/airsim-data/mixed/rain-10",
+        "data/mcai/airsim-data/mixed/rain-100",
+        "data/mcai/airsim-data/mixed/rain-1000",
+        "data/mcai/airsim-data/mixed/maple_leaf-10",
+        "data/mcai/airsim-data/mixed/maple_leaf-100",
+        "data/mcai/airsim-data/mixed/maple_leaf-1000",
     ]
     checkpoints = [
         # Peking University
