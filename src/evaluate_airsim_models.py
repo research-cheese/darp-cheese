005c1a3859cbee27d29b9202b70040a02bacb6e3
hwelsters <hwelsters>
2024-09-08 16:56:11 -0700

    a hard day's work

--- src/evaluate_airsim_models.py
+++ src/evaluate_airsim_models.py
@@ -17,26 +17,26 @@ for eval_dataset in [
         output_dir_base_model_path = os.path.join(output_dir_base, "model.pth")
 
         evaluate_base_model(
-            checkpoint=checkpoint,
+            checkpoint=output_dir_base_model_path,
             id2label=AirsimObjectDetectionConfig.id2label,
             label2id=AirsimObjectDetectionConfig.label2id,
             dataset_dir=eval_dataset,
         )
 
-        for dataset in AirsimObjectDetectionConfig.datasets:
+        # for dataset in AirsimObjectDetectionConfig.datasets:
 
-            output_dir = os.path.join(
-                "output",
-                dataset,
-                checkpoint
-            )
+        #     output_dir = os.path.join(
+        #         "output",
+        #         dataset,
+        #         checkpoint
+        #     )
 
-            evaluate_base_model(
-                checkpoint=output_dir_base_model_path,
-                id2label=AirsimObjectDetectionConfig.id2label,
-                label2id=AirsimObjectDetectionConfig.label2id,
-                dataset_dir=eval_dataset,
-            )
+        #     evaluate_base_model(
+        #         checkpoint=output_dir_base_model_path,
+        #         id2label=AirsimObjectDetectionConfig.id2label,
+        #         label2id=AirsimObjectDetectionConfig.label2id,
+        #         dataset_dir=eval_dataset,
+        #     )
 
             # output_base_model_path = os.path.join(output_dir_base, "model.pth")
             
