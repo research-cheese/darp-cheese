3345e0d7f3bdc7e8c138b0ec362b06b4648c42c1
hwelsters <hwelsters>
2024-09-09 13:36:38 -0700

    a hard day's work

--- gracehopper.sh
+++ gracehopper.sh
@@ -5,7 +5,7 @@
 #SBATCH -t 7-00:00:00   # time in d-hh:mm:ss
 #SBATCH -G 1            # number of GPU 
 #SBATCH -N 1            # number of nodes
-#SBATCH -c 8            # number of cores 
+#SBATCH -c 1            # number of cores 
 #SBATCH -o slurms/slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurms/slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
