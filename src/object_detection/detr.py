c080a613c971de9d94f5adc7f02bc473a9f6f258
hwelsters <hwelsters>
2024-09-11 12:34:32 -0700

    crafting building.

--- src/object_detection/detr.py
+++ src/object_detection/detr.py
@@ -292,7 +292,7 @@ def compute_metrics(evaluation_results, image_processor, threshold=0.0, id2label
 import wandb
 os.environ["WANDB_PROJECT"] = "nngu2-mcai"
 os.environ["WANDB_LOG_MODEL"] = "checkpoint"
-os.environ["HF_DATASETS_CACHE"] = "/scractch/nngu2/hf-datasets"
+os.environ["HF_DATASETS_CACHE"] = "/scratch/nngu2/hf-datasets"
 def build_trainer(
     model, image_processor, train_dataset, id2label, eval_dataset, output_path
 ) -> Trainer:
