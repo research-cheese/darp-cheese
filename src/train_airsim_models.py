6f81654fd99f7355c5b5b4c7c6ceb4ba3975ffcd
hwelsters <hwelsters>
2024-09-11 04:08:05 -0700

    crafting building.

--- src/train_airsim_models.py
+++ src/train_airsim_models.py
@@ -11,8 +11,6 @@ from training_configs import AirsimObjectDetectionConfig
 
 for checkpoint in AirsimObjectDetectionConfig.checkpoints:
     output_dir_base = os.path.join(
-        "/scratch",
-        "nngu2",
         "output",
         AirsimObjectDetectionConfig.base_dataset,
         checkpoint,
@@ -47,7 +45,7 @@ for checkpoint in AirsimObjectDetectionConfig.checkpoints:
 
     for dataset in AirsimObjectDetectionConfig.datasets:
 
-        output_dir = os.path.join("/scratch", "nngu2", "output", dataset, checkpoint)
+        output_dir = os.path.join("output", dataset, checkpoint)
         output_dir_base_dataset_path = os.path.join(output_dir, "base")
         output_dir_base_dataset_model_path = os.path.join(
             output_dir, "base", "model.pth"
