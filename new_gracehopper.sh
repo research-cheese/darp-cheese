978a1df8062391463c74d731b95175bfd2fa679f
hwelsters <hwelsters>
2024-09-12 12:52:10 -0700

    crafting building.

--- new_gracehopper.sh
+++ new_gracehopper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH -p general      # partition 
-#SBATCH -q public # queue
-#SBATCH -t 1-00:00:00   # time in d-hh:mm:ss
+#SBATCH -L gracehopper
+#SBATCH -p highmem      # partition 
+#SBATCH -q grp_scai_research_priority # queue
+#SBATCH -t 7-00:00:00   # time in d-hh:mm:ss
 #SBATCH -G 1            # number of GPU 
 #SBATCH -N 1            # number of nodes
-#SBATCH -c 8            # number of cores 
+#SBATCH -c 1            # number of cores 
 #SBATCH -o slurms/slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurms/slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
