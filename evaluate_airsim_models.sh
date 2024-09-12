6318b09bec59f92416378187bdf23e63e9417d37
hwelsters <hwelsters>
2024-09-12 11:00:06 -0700

    crafting building.

--- evaluate_airsim_models.sh
+++ evaluate_airsim_models.sh
@@ -1,2 +1,4 @@
 source venv/bin/activate
+
+echo "Evaluate AirSim models"
 python3 src/evaluate_airsim_models.py
--- train_airsim_models.sh
+++ train_airsim_models.sh
@@ -1,2 +1,4 @@
 source venv/bin/activate
+
+echo "Training AirSim models"
 python3 src/train_airsim_models.py
