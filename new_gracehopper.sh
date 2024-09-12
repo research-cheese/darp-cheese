10397b1647beb58fd4370ee27800112e65f138e8
hwelsters <hwelsters>
2024-09-12 12:51:37 -0700

    crafting building.

--- new_gracehopper.sh
+++ new_gracehopper.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
+#SBATCH -p general      # partition 
+#SBATCH -q public # queue
+#SBATCH -t 1-00:00:00   # time in d-hh:mm:ss
+#SBATCH -G 1            # number of GPU 
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 8            # number of cores 
+#SBATCH -o slurms/slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurms/slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user="%nngu2@asu.edu"
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
 rm -rf venv
 source gracehopper.sh
