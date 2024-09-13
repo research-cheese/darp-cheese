d23a733e1578d88e8d3717bd06228ade70c02760
hwelsters <hwelsters>
2024-09-13 12:05:41 -0700

    forgot a comma, lol

--- src/object_detection/detr.py
+++ src/object_detection/detr.py
@@ -318,8 +318,8 @@ def build_trainer(
         remove_unused_columns=False,
         eval_do_concat_batches=False,
         push_to_hub=False,
-        report_to=["wandb"],
-        resume_from_checkpoint=True
+        # report_to=["wandb"],
+        # resume_from_checkpoint=True
     )
 
     eval_compute_metrics_fn = partial(
