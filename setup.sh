eb4d40dbba720c0539580a8d38f8504075735365
hwelsters <hwelsters>
2024-09-16 18:02:43 -0700

    forgot a comma, lol

--- setup.sh
+++ setup.sh
@@ -7,37 +7,21 @@ export WANDB_CACHE_DIR="/scratch/nngu2/.cache/wandb"
 export WANDB_CONFIG_DIR="/scratch/nngu2/.config/wandb"
 export WANDB_MODE=online
 
-conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia
-conda install peft
-conda install datasets
-conda install transformers[torch]
-conda install pillow
-conda install albumentations
-conda install timm
-conda install torchmetrics
-conda install dataclasses
-conda install numpy
-conda install pycocotools
-conda install transformers
-conda install wandb
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+pip install peft
 
+# datasets
+pip install datasets
 
+# transformers
+pip install transformers[torch]
 
-# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
-# pip install peft
-
-# # datasets
-# pip install datasets
-
-# # transformers
-# pip install transformers[torch]
-
-# pip install pillow
-# pip install albumentations
-# pip install timm
-# pip install torchmetrics
-# pip install dataclasses
-# pip install numpy
-# pip install pycocotools
-# pip install transformers
-# pip install wandb
+pip install pillow
+pip install albumentations
+pip install timm
+pip install torchmetrics
+pip install dataclasses
+pip install numpy
+pip install pycocotools
+pip install transformers
+pip install wandb
