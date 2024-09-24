941b5d5d74db95903648c892fd6fbc3add9eabbb
hwelsters <hwelsters>
2024-09-24 10:20:53 -0700

    a

--- train_airsim_models.sh
+++ train_airsim_models.sh
@@ -1,2 +1,4 @@
 python src/train_airsim_models.py
 python src/mariofy.py
+
+apptainer shell --nv --bind /home/nngu2:/home/nngu2 --bind /scratch/nngu2:/scratch/nngu2 /packages/aarch64/simg/pytorch_24.05-py3.sif
